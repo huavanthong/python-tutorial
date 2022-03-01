85c1e1482f5fe8f26fc7bd4232a8ab98acf780be
huavanthong <50081052+huavanthong@users.noreply.github.com>
2022-03-01 10:30:10 +0700

    Create a module Person

---
+++ 01_GettingStarted/00_Samples/Person.py
@@ -0,0 +1,20 @@
+
+# Global variable
+pi = 3.14
+
+# show function 
+def plusOne():
+    pi = pi + 1
+    print(pi)
+
+# Class Person
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def setName(self, name):
+    self.name = name
+  
+  def setAge(self, age):
+    self.age = age
